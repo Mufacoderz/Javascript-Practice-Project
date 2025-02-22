2579f4c6edbdd585a85123872eed2191348eba8e
Muhammad <mf9555759@gmail.com>
2025-02-22 21:26:02 +0800

    menghubungkan elemen html ke js

---
+++ Countdown Timer/script.js
@@ -0,0 +1,11 @@
+
+
+const countdownElement = document.getElementById("countdown")
+const daysElement = document.getElementById("days")
+const hoursElement = document.getElementById("hours")
+const minutesElement = document.getElementById("minutes")
+const secondsElement = document.getElementById("seconds")
+const inputHours = document.getElementById("inputHours")
+const inputMinutes = document.getElementById("inputMinutes")
+const inputSeconds = document.getElementById("inputSeconds")
+const startButton = document.getElementById("startButton")
