41c2b28693edd1f21c7030fac295ab4930adc1a5
Muhammad <mf9555759@gmail.com>
2025-02-23 21:02:29 +0800

    scripting

--- Countdown Timer/script.js
+++ Countdown Timer/script.js
@@ -9,3 +9,45 @@ const inputHours = document.getElementById("inputHours")
 const inputMinutes = document.getElementById("inputMinutes")
 const inputSeconds = document.getElementById("inputSeconds")
 const startButton = document.getElementById("startButton")
+
+let countdownInterval;
+
+function startTimer(){
+    let hours = parseInt(inputHours.value) || 0
+    let minutes = parseInt(inputMinutes.value) || 0
+    let seconds = parseInt(inputSeconds.value) || 0
+
+    let totalTimeInSeconds = hours * 3600 + minutes * 60 + seconds
+
+    if (totalTimeInSeconds <=0){
+        alert("please enter a valid time")
+        return
+    }
+    inputHours.value = ''
+    inputMinutes.value = ''
+    inputSeconds.value = ''
+
+    countdownInterval = setInterval(()=>{
+        const days = Math.floor(totalTimeInSeconds/86400)
+        const hours = Math.floor((totalTimeInSeconds/86400) / 3600)
+        const minutes = Math.floor((totalTimeInSeconds%3600)/60)
+        const seconds = Math.floor((totalTimeInSeconds%60))
+
+        daysElement.textContent = days.toString().padStart(2, '0')
+        hoursElement.textContent = hours.toString().padStart(2, '0')
+        minutesElement.textContent = minutes.toString().padStart(2, '0')
+        secondsElement.textContent = seconds.toString().padStart(2, '0')
+
+        totalTimeInSeconds --
+
+        if(totalTimeInSeconds < 0){
+            clearInterval(countdownInterval)
+            alert('Times up!')
+        }
+    }, 1000)
+}
+
+startButton.addEventListener('click', ()=>{
+    clearInterval(countdownInterval)
+    startTimer()
+})
