fd1b4e2ca7364e9e140b92224b01e6b550a95c1d
Muhammad <mf9555759@gmail.com>
2025-03-09 20:35:20 +0800

    get elemen html

---
+++ Number Guessing/script.js
@@ -0,0 +1,9 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1
+
+let attemps = 0
+
+const guessInput = document.getElementById('guessInput')
+const guessButton = document.getElementById('guessButton')
+const message = document.getElementById('message')
+const restartButton = document.getElementById('restartButton')
+
