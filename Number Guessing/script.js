a764a9c897823f7d5d9ceb7639c8b13f9202ef93
Muhammad <mf9555759@gmail.com>
2025-03-09 20:43:06 +0800

    scripting

--- Number Guessing/script.js
+++ Number Guessing/script.js
@@ -1,9 +1,34 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1
 
-let attemps = 0
+let attempts = 0
 
 const guessInput = document.getElementById('guessInput')
 const guessButton = document.getElementById('guessButton')
 const message = document.getElementById('message')
 const restartButton = document.getElementById('restartButton')
 
+function checkGuess(){
+    const userGuess = Number(guessInput.value)
+    attempts++
+
+    if( userGuess === randomNumber){
+        message.textContent = `Congratulations! You guessed the Number ${randomNumber} correctly in ${attempts} attempts`
+        message.style.color = '#28a745'
+        endGame()
+    } else if (userGuess > randomNumber) {
+        message.textContent = "To High! Try Again"
+        message.style.color = '#dc3545'
+    } else if (userGuess < randomNumber){
+        message.textContent = "To Low! Try Again"
+        message.style.color = '#dc3545'
+    }
+
+    guessInput = ''
+    guessInput.focus()
+}
+
+function endGame(){
+    guessInput.disable = true
+    guessButton.disable = true
+    restartButton.style.display = 'inline'
+}
