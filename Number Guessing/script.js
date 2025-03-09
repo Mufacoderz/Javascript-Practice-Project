21da6c2e505c488d66c6dd1b7344f4baf1f57807
Muhammad <mf9555759@gmail.com>
2025-03-09 21:20:02 +0800

    finis

--- Number Guessing/script.js
+++ Number Guessing/script.js
@@ -32,3 +32,25 @@ function endGame(){
     guessButton.disable = true
     restartButton.style.display = 'inline'
 }
+
+function resetGame(){
+    attempts = 0
+    randomNumber = Math.floor(Math.random() * 100) + 1
+    guessInput.disable = false
+    guessButton.disable = false
+    message.textContent = 'Good Luck! Start Guess . . .'
+    message.style.color = '#333333'
+    restartButton.style.display = 'none'
+    guessInput.value = ''
+    guessInput.focus()
+}
+
+guessButton.addEventListener('click', checkGuess)
+
+restartButton.addEventListener('click', resetGame)
+
+guessInput.addEventListener('keydown', function(event){
+    if (event.key === 'Enter'){
+        checkGuess()
+    }
+})
