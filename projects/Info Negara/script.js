0dc9e6728b65a42a0af1d20cbae4b9b21a6ea300
Muhammad <mf9555759@gmail.com>
2025-06-19 19:28:36 +0800

    done info negara

---
+++ projects/Info Negara/script.js
@@ -0,0 +1,39 @@
+const form = document.getElementById('search-form');
+const input = document.getElementById('country-input');
+const resultContainer = document.getElementById('result');
+const flagImg = document.getElementById('flag');
+const countryName = document.getElementById('country-name');
+const capital = document.getElementById('capital');
+const population = document.getElementById('population');
+const currency = document.getElementById('currency');
+const languages = document.getElementById('languages');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (input.value != '') {
+    searchCountry(input.value.trim());
+  }
+});
+
+const searchCountry = (namaNegara) => {
+  fetch(`https://restcountries.com/v3.1/name/${namaNegara}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      
+      const negara = data[0];
+
+      flagImg.src = negara.flags.svg;
+      countryName.innerText = negara.name.common;
+      capital.innerText = negara.capital[0];
+      population.innerText = negara.population.toLocaleString();
+      currency.innerText = Object.values(negara.currencies)[0].name;
+      languages.innerText = Object.values(negara.languages).join(', ');
+
+      resultContainer.classList.remove('hidden');
+    })
+    .catch(error => {
+      console.error('Terjadi kesalahan:', error);
+      alert('Negara tidak ditemukan!');
+    });
+}
